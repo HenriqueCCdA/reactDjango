b9ec57689b96be548bf3e613bddb640ec3198534
Henrique Andrade <henrique.ccda@gmail.com>
2022-07-14 22:23:00 -0300

    Token.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,6 @@
 from django.views import View
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import permissions, authentication
 from .serializers import ListSerializer, ItemSerializer
 from .models import Item, List
 
@@ -9,9 +9,11 @@ class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
     permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
 
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permission_classes =  [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
