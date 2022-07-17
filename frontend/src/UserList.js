5c45a27c77793bc94016f19bfc3b6f849454b67a
Henrique Andrade <henrique.ccda@gmail.com>
2022-07-17 17:07:42 -0300

    Fazendo o fecth para API

--- frontend/src/UserList.js
+++ frontend/src/UserList.js
@@ -4,8 +4,15 @@ import ListComponent from "./ListComponent";
 export default class UserLists extends React.Component{
     state = { lists: null, loading: true }
     async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token 2f8a6be50af1e6c1ab3fddd17dd82f92e90c8ab8'
+
         var url = 'http://127.0.0.1:8000/list/';
-        const response = await fetch(url);
+        const response = await fetch(url, config);
         const data = await response.json();
         console.log(data)
         this.setState({lists: data, loading: false});
